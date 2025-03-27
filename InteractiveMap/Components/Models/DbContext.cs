b2a4dc8ee7cb7c104ce45e7f5b38a279101ef976
Aysh Khan <ayshkhan12122@gmail.com>
2025-03-27 13:05:25 +0000

    added DBContext file as well as changed the program.cs code

---
+++ InteractiveMap/Components/Models/DbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InteractiveMap.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite("Data Source=InteractiveMap.db");
+            }
+        }
+    }
+}
